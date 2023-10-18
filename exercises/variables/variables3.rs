ff4f0489d4570069cc4d5b3648b5309ffdeaad73
overfpga <897174113@qq.com>
2023-10-18 13:43:30 +0800

    variables

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -6,6 +6,15 @@
 // I AM NOT DONE
 
 fn main() {
-    let x: i32;
+    let x: i32 = 123;
     println!("Number {}", x);
+    // println!("Number {x}");
 }
+
+
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
